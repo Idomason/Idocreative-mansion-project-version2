ca1ae5c02222593fd66fa0d683ea6f813e21a768
Idoma Ngbede <idomakevin@gmail.com>
2022-10-28 18:46:01 +0100

    new update added today

--- js/project-slider.js
+++ js/project-slider.js
@@ -1,4 +1,15 @@
-// Select all needed tags
+// Selected all needed tags
+const phoneIcon = document.querySelector('.phone-icon');
+const phoneNumber = document.querySelector('.phone-number');
+const navIcons = document.querySelector('.nav-icons');
+const navbar = document.querySelector('.navbar');
+const navItems = document.querySelector('.nav-items');
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.close-menu');
+const menuItems = document.querySelector('.navbar .menu-items li .nav-link');
+const menuItems2 = document.querySelector('.navbar2');
+
+// Image Slider
 const next = document.querySelector('.next-icon');
 const prev = document.querySelector('.previous-icon');
 const imgcontainer = document.querySelector('.project-images');
@@ -28,22 +39,36 @@ function updateImage() {
     currImg = numImg;
   }
   imgcontainer.style.transform = `translateX(-${(currImg - 1) * 800}px)`;
-
-  timeoutID = setTimeout(() => {
-    currImg++;
-    updateImage();
-  }, 5000);
 }
 
-updateImage();
-
 // ==================  PHONE ===============
-const phoneIcon = document.querySelector('.phone-icon');
-const phoneNumber = document.querySelector('.phone-number');
-const navIcons = document.querySelector('.nav-icons');
+
 console.log('No');
 phoneIcon.addEventListener('click', function () {
   console.log('clicked');
   navIcons.style.width = '80%';
   phoneNumber.classList.toggle('show-number');
 });
+
+// ================  STICKY NAVBAR ================
+
+window.addEventListener('scroll', function () {
+  navbar.classList.toggle('sticky', window.scrollY > 0);
+  menuItems2.classList.add('close');
+});
+
+// ================  HAMBURGER ================
+// Open function
+function open() {
+  menuItems2.classList.remove('close');
+  menuItems2.classList.add('open');
+}
+
+// Close function
+function close() {
+  menuItems2.classList.add('close');
+}
+
+openMenu.addEventListener('click', open);
+
+closeMenu.addEventListener('click', close);
